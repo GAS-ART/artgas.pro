30018f4977716c29abdb33c59515c49580aa9e86
Artem <temoha1386@gmail.com>
2022-08-09 10:02:41 +0300

    Turn off unnecessary connections

--- resources/views/inc/head.blade.php
+++ resources/views/inc/head.blade.php
@@ -25,21 +25,18 @@
    <meta name='freelancehunt' content='9404d1757b7c150'>
 
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-   <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
-   <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+   <!--<link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+   <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />-->
   
       <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Montserrat:ital,wght@0,400;0,500;0,600;1,600&display=swap" rel="stylesheet">
 
    <link rel="stylesheet" href="{{asset('css/style.css') . '?_v=9'}}"> 
-   @if(Request::is('furino'))
-   <link rel="stylesheet" href="{{asset('css/furino.css')}}">
-   @endif
 
    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-   <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
-   <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+  <!-- <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+   <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script> -->
    <script src="{{asset('js/app.js')}}"></script>
 
    <!-- Global site tag (gtag.js) - Google Analytics -->
